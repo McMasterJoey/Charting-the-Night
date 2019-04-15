1d3cb2c6c10a2922849088444eb6106ccc656797
JCSCMaster <jmcmaster@email.arizona.edu>
2019-04-15 16:43:25 -0700

    Restored an older file

--- src/Model/CTS_Constellation.java
+++ src/Model/CTS_Constellation.java
@@ -1,9 +1,27 @@
 package Model;
 
-public class CTS_Constellation {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
-	public CTS_Constellation() {
-		// TODO Auto-generated constructor stub
+/**
+ * The CTS_Constellation is a class that holds all the associated data with a constellation. 
+ * @author Joey McMaster
+ * @author Nicholas Fiegel
+ * @author Matt Theisen
+ * @author Jackson
+ *
+ */
+public class CTS_Constellation extends CTS_SpaceObject {
+	private List<CTS_Star> _stars;
+	private List<CTS_Constellation_Line> _lines;
+	private String _name = "Default Constellation Name";
+	public CTS_Constellation(String name, double rightAcension, double declination) {
+		super(name, rightAcension, declination);
+		_name = name;
 	}
-
+	public CTS_Constellation(String name, double rightAcension, double declination, double altitude, double azimuth, Collection<CTS_Star> stars) {
+		super(name, rightAcension, declination, altitude, azimuth);
+		_name = name;
+	}
 }
