544628a2dca0d2a6e98b356f4f2a5882bf0918a1
JCSCMaster <jmcmaster@email.arizona.edu>
2019-04-15 16:37:03 -0700

    Got a window to popup and added a little icon

--- src/View/CTS_GUI.java
+++ src/View/CTS_GUI.java
@@ -1,4 +1,11 @@
 package View;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
 /**
  * Place holder GUI class for the project
  * Please edit once we do something meaningful with it.
@@ -8,9 +15,23 @@ package View;
  * @author Jackson
  *
  */
-public class CTS_GUI {
+public class CTS_GUI extends Application {
 
 	public CTS_GUI(String[] args) {
+		launch(args);
+	}
+	public CTS_GUI() {
+		
+	}
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.setTitle("Charting The Stars");
+		stage.getIcons().add(new Image(getClass().getResourceAsStream("icon.png")));
+		BorderPane mainpane = new BorderPane();
+		System.out.println("Display!");
+		Scene scene = new Scene(mainpane, 600,600);
+        stage.setScene(scene);
+        stage.show();
 	}
 
 }
