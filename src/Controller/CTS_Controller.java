8599b4ab687430a7830106631d3167726e283064
theisenm <theisenm@email.arizona.edu>
2019-04-15 21:06:11 -0700

    Controller edited to read Stars.csv file

--- src/Controller/CTS_Controller.java
+++ src/Controller/CTS_Controller.java
@@ -1,4 +1,13 @@
 package Controller;
+import Model.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Place holder controller for the project
  * Please edit once we do something meaningful with it.
@@ -10,8 +19,42 @@ package Controller;
  */
 public class CTS_Controller {
 
+	ArrayList<CTS_Star> starList;
+	
+	
 	public CTS_Controller() {
+		build_starList();
 	}
 
+	/**
+	 * Parse Stars.csv database to build list of stars
+	 */
+	private void build_starList() {
+		
+		BufferedReader fileReader = null;
+		
+		try {
+			fileReader = new BufferedReader(new FileReader(".\\src\\Resources\\Stars.csv"));
+			String line = "";
+			String[] tokens;
+		    
+			// Read lines of Stars.csv file
+		    while ((line = fileReader.readLine()) != null)
+		    {
+		    	//Get all tokens available in line
+		        tokens = line.split(",");
+		        System.out.println(line);
+		   
+		    }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	    
+	    try {
+            fileReader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
 	
 }
