2ad65d514d4cbc84eeea75ad00db4de3e3dd6109
Nicholas Fiegel <nicholasfiegel@email.arizona.edu>
2019-04-16 10:08:11 -0700

    Updated getLocalSiderialTime and getHourAngle to use loops instead of if statments. Added javadoc.

--- src/Model/CTS_Star.java
+++ src/Model/CTS_Star.java
@@ -52,43 +52,68 @@ public class CTS_Star extends CTS_SpaceObject {
 		return magnitude;
 	}
 
+	/**
+	 * Returns the local siderial time for the star.
+	 * ASSUMES: daysSinceStand is set to the decimal days since J2000
+	 * ASSUMES: universalTime is appropriately set
+	 * @return a double representing the local siderial time for the star
+	 */
 	public double getLocalSiderialTime() {
 		double lst = 100.46 + 0.985647 * daysSinceStandard + 15 * universalTime;
 
-		if (lst < 0) {
+		while (lst < 0) {
 			lst += 360;
 		}
-		else if (lst > 360) {
+
+		while (lst > 360) {
 			lst -= 360;
 		}
 
 		return lst;
 	}
 
+	/**
+	 * Calculates and returns the hour angle.
+	 * USES: getLocalSiderialTime, so the assumptions in getLocalSiderialTime
+	 * are in full affect for this method.
+	 * @return a double representing the hour angle
+	 */
 	public double getHourAngle() {
 		double lst = getLocalSiderialTime();
 		double ha = lst - rightAscension;
 
-		if (ha < 0) {
+		while (ha < 0) {
 			ha += 360;
 		}
-		else if (ha > 360) {
+
+		while (ha > 360) {
 			ha -= 360;
 		}
 
 		return lst - rightAscension;
 	}
 
+	/**
+	 * Calculates and sets the altitude for the star.
+	 * USES: getHourAngle, so transitively the assumptions in getLocalSiderialTime
+	 * are in full affect for this method.
+	 */
 	public void calcAltitude() {
 		double sinOfAlt = Math.sin(declination) * Math.sin(latitude) + Math.cos(declination) * Math.cos(latitude) * Math.cos(getHourAngle());
 
 		altitude = Math.asin(sinOfAlt);
 	}
 
+	/**
+	 * Calculates and sets the azimuth for the star.
+	 * USES: calcAltitude, so transitively the assumptions in getLocalSiderialTime
+	 * are in full affect for this method.
+	 */
 	public void calcAzimuth() {
 		calcAltitude();
 		azimuth = Math.cos(altitude);
 	}
+
 	/**
 	 * Makes the object printable easily.
 	 * @return The string representation of the CTS_Star object.
