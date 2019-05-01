b5272842c0f5ee1dec19ad7a688cff9c8fc9b594
Jtroyer <Jtroyer@172.16.4.255>
2019-05-01 13:21:10 -0700

    first logic

--- src/Model/CTS_Planet.java
+++ src/Model/CTS_Planet.java
@@ -5,19 +5,28 @@ import java.lang.Math;
 public class CTS_Planet extends CTS_SpaceObject{
 	
 	
-	public CTS_Planet(double U, double W, double V, double L, String nameIn, int setID) {
+	public CTS_Planet(String nameIn, int setID, double M, double e, double a) {
 		super (setID, nameIn, 0.0, 0.0);
-		L = 2 * Math.PI * L;
 		
-		this.rightAscension = U - Math.pow(V, 2.0);
-		this.rightAscension = Math.sqrt(this.rightAscension);
-		this.rightAscension = W / this.rightAscension;
-		this.rightAscension = 1/Math.sin(this.rightAscension);
-		this.rightAscension = this.rightAscension + L;
+		M = Math.toRadians(M);
+		double E = M + e * Math.sin(M) * (1.0 + e * Math.cos(M));
+		//Anomaly convergence 
+		if (e > 0.05) {
+			double E0 = E;
+			double E1 = E0 - (E0 - e * Math.sin(E0) - M) / (1 -e * Math.cos(E0));
+			while (Math.abs(E0 - E1) > 0.06) {
+				E0 = E1;
+				E1 = E0 - (E0 - e * Math.sin(E0) - M) / (1 -e * Math.cos(E0));
+			}
+			E = E0;
+		}
 		
-		this.declination = V / Math.sqrt(U);
-		this.declination = 1/Math.sin(this.declination);
-
+		double xv = a * (Math.cos(E) - e);
+		double yv = a * (Math.sqrt(1.0 - e*e) * Math.sin(E));
+		
+		double v = Math.atan2(yv, xv);
+		double r = Math.sqrt(xv*xv + yv*yv);
+		
 		
 		
 	}
