a23200629ad2ffb0f91b50c14c038c40c09bc8ce
JCSCMaster <jmcmaster@email.arizona.edu>
2019-05-01 12:05:33 -0700

    Added javadoc to deep space objects, eliminated unused classes.

--- src/Model/CTS_DeepSkyObject.java
+++ src/Model/CTS_DeepSkyObject.java
@@ -8,11 +8,28 @@ package Model;
  *
  */
 public class CTS_DeepSkyObject extends CTS_SpaceObject {
-
+	/**
+	 * The primary constructor of the deep space object.
+	 * @param id The id of the deep space object.
+	 * @param name The name of the deep space object.
+	 * @param magnitude The magnitude of the deep space object.
+	 * @param rightAcension The rightAcension of the deep space object.
+	 * @param declination The declination of the deep space object.
+	 */
 	public CTS_DeepSkyObject(int id, String name, double magnitude, double rightAcension, double declination) {
 		super(id ,name, rightAcension, declination);
 		this.magnitude = magnitude;
 	}
+	/**
+	 * The secondary constructor of the deep space object.
+	 * @param id The id of the deep space object.
+	 * @param name The name of the deep space object.
+	 * @param magnitude The magnitude of the deep space object.
+	 * @param rightAcension The rightAcension of the deep space object.
+	 * @param declination The declination of the deep space object.
+	 * @param altitude The altitude of the deep space object.
+	 * @param azimuth The azimuth of the deep space object.
+	 */
 	public CTS_DeepSkyObject(int id, String name, double magnitude, double rightAcension, double declination, double altitude, double azimuth) {
 		super(id ,name, rightAcension, declination, altitude, azimuth);
 		this.magnitude = magnitude;
